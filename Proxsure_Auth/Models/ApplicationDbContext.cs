df66a2118882aa534b7e17b686f1ebae20bdeb65
Iyanu Akenroye <iyanuakenroye.gmail.com>
2018-10-11 12:12:42 +0100

    setting up projects

---
+++ Proxsure_Auth/Models/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Proxsure_Auth.Models {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> 
+    {
+
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base (options) {
+
+        }
+
+         protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+           
+            // Add your customizations after calling base.OnModelCreating(modelBuilder);
+            builder.Entity<ApplicationUser> ().ToTable ("User");
+            builder.Entity<IdentityRole> ().ToTable ("Role");
+            builder.Entity<IdentityUserClaim<string>> ().ToTable ("UserClaim");
+            builder.Entity<IdentityUserRole<string>> ().ToTable ("UserRole");
+            builder.Entity<IdentityUserLogin<string>> ().ToTable ("UserLogin");
+            builder.Entity<IdentityRoleClaim<string>> ().ToTable ("RoleClaim");
+            builder.Entity<IdentityUserToken<string>> ().ToTable ("UserToken");
+        }
+    }
+}
---
+++ Proxsure_Auth/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Proxsure_Auth.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
